d94d5cfc0a32fca53ee2c37e90244666b883d92d
Pann Sengpanich <panncraftyo@gmail.com>
2023-02-19 11:51:40 +0700

    update new coding file

--- Javascript/Projects/Project.js
+++ Javascript/Projects/Project.js
@@ -93,6 +93,7 @@ const checkCashRegister = (price, cash, cid) => {
     TWENTY: 20.0,
     "ONE HUNDRED": 100.0,
   };
+
   let totalCID = 0;
   for (let element of cid) {
     totalCID += element[1];
@@ -100,8 +101,10 @@ const checkCashRegister = (price, cash, cid) => {
   totalCID = totalCID.toFixed(2);
   let changeToGive = cash - price;
   const changeArray = [];
+
+  //check
   if (changeToGive > totalCID) {
-    return { status: "INSUFFICIENT_FUNDS", change: changeArray };
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
   } else if (changeToGive.toFixed(2) === totalCID) {
     return { status: "CLOSED", change: cid };
   } else {
@@ -114,24 +117,21 @@ const checkCashRegister = (price, cash, cid) => {
         changeToGive -= UNIT_AMOUNT[elem[0]];
         changeToGive = changeToGive.toFixed(2);
       }
-      if (temp[1] > 0) {
-        changeArray.push(temp);
-      }
+      changeArray.push(temp);
     }
-  }
-  if (changeToGive > 0) {
-    return { status: "INSUFFICIENT_FUNDS", change: [] };
   }
   return { status: "OPEN", change: changeArray };
 };
-checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-]);
+console.log(
+  checkCashRegister(19.5, 200, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
