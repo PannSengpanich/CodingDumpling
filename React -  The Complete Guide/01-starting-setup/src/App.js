572e4f08f8596f6c6daf570f5990cfe2e0754aca
Pann Sengpanich <panncraftyo@gmail.com>
2023-04-20 11:21:56 +0700

    update react file

--- React -  The Complete Guide/01-starting-setup/src/App.js
+++ React -  The Complete Guide/01-starting-setup/src/App.js
@@ -1,10 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
-}
+} //component is just a JS function, can only have 1 root element
 
 export default App;
