562ee906b054eee85f7980f273497e07e6ffb62b
Pann Sengpanich <panncraftyo@gmail.com>
2023-04-20 23:25:02 +0700

    progress React course

--- React -  The Complete Guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React -  The Complete Guide/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import React, { useState } from "react"; //define a state variable and a function that can be used to update the state variable
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title); //! [name for state variable, function to update the state variable] = useState(initial value for state variable); (cant be called outside component function)
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <div>
         <p>other info: {props.info}</p>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 } //component
