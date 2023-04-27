1bed76ff0bc190b719fba589a464ff53400d7a6c
Pann Sengpanich <panncraftyo@gmail.com>
2023-04-27 16:27:09 +0700

    update webpack-demo

--- Odin Projects/Full Stack JavaScript/JavaScript/webpack-demo/webpack.config.js
+++ Odin Projects/Full Stack JavaScript/JavaScript/webpack-demo/webpack.config.js
@@ -1,9 +1,65 @@
 const path = require("path");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+    another: "./src/another-module.js",
+  }, //2 entries
+  devtool: "inline-source-map", //use to track down errors
+  devServer: {
+    static: "./dist", // live reloading.
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js", //2 outputs
+    path: path.resolve(__dirname, "dist"), //output to dist
+    clean: true,
+    publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" },
+      { test: /\.(csv|tsv)$/i, use: ["csv-loader"] },
+      { test: /\.xml$/i, use: ["xml-loader"] },
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
   },
 };
