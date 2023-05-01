1631724620b99ec4e2bee7f83534c5377685bf0c
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-01 20:05:15 +0700

    Update asynchronous.js

--- Odin Projects/Full Stack JavaScript/JavaScript/asynchronous.js
+++ Odin Projects/Full Stack JavaScript/JavaScript/asynchronous.js
@@ -1,10 +1,11 @@
-//!Promises
+//!Promises (asynchronous operation) (use to fetch data and read files)
 
+//TODO: order: sync ---> async(resolve,reject ---> finally)
 let p1 = new Promise((resolve, reject) => {
-  let a = 1 + 1;
-  if (a == 2) {
-    resolve("Succeed");
-  } else reject("Failed");
+  let a = 1000 + 1000;
+  if (a == 2000) {
+    resolve("p1 Succeed");
+  } else reject("p1 Failed");
 });
 //return either resolve or reject
 // if resolve -> then, if reject -> catch
@@ -15,27 +16,37 @@ p1.then((message) => {
   .catch((message) => {
     console.log("Catch: " + message);
   })
-  .finally(() => console.log("execute everytime"));
+  .finally(() => console.log("execute everytime")); //execute after every then and catch of this file
 
 console.log(p1); // Promise { 'Succeed' }
 
 let p2 = new Promise((resolve, reject) => {
-  let a = 1 + 2;
+  let a = 2 + 2;
   if (a == 4) {
-    resolve("Succeed");
-  } else reject("Failed");
+    resolve("p2 Succeed");
+  } else reject("p2 Failed");
 });
 
 console.log(p2); // Promise { <rejected> 'Failed' }
 
 Promise.all([p1, p2])
-  .then(function () {
-    console.log("Both promises resolved");
+  .then(function (result) {
+    // result is values from resolve
+    console.log(`Both promises resolved + ${result}`);
+    console.log(result);
   })
-  .catch(function () {
-    console.log("One or more promises was rejected");
+  .catch(function (err) {
+    // rejected immediately after the first rejected
+    console.log(`One or more promises was rejected + ${err}`);
   });
 
+Promise.race([p1, p2]) //   triggers as soon as any promise in the array is resolved or rejected
+  .then(function (result) {
+    console.log(`Both promises resolved + ${result}`);
+  })
+  .catch(function (err) {
+    console.log(`One or more promises was rejected + ${err}`);
+  });
 //* the asynchronous code will be executed after
 //* the synchronous code has finished executing
 //* ex. console.log() is sync , resolve/reject is async
