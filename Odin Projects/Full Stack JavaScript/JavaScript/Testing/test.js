dd1160846d160f3e8e8a4c620f4648c26c4ff46e
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-12 22:09:49 +0700

    Update test.js

--- Odin Projects/Full Stack JavaScript/JavaScript/Testing/test.js
+++ Odin Projects/Full Stack JavaScript/JavaScript/Testing/test.js
@@ -7,11 +7,11 @@ test("adding positive numbers is not zero", () => {
 });
 test("null", () => {
   const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+  expect(n).toBeNull(); //matches null
+  expect(n).toBeDefined(); //matches undefined
+  expect(n).not.toBeUndefined(); //matches not undefined
+  expect(n).not.toBeTruthy(); //matches anything that an if statement treats as true
+  expect(n).toBeFalsy(); //matches anything that an if statement treats as false
 });
 test("adding floating point numbers", () => {
   const value = 0.1 + 0.2;
@@ -20,4 +20,34 @@ test("adding floating point numbers", () => {
 });
 test('but there is a "stop" in Christoph', () => {
   expect("Christoph").toMatch(/stop/);
+});
+
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
+});
+
+function compileAndroidCode() {
+  throw new Error("you are using the wrong JDK!");
+}
+
+test("compiling android goes as expected", () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use a string that must be contained in the error message or a regexp
+  expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+
+  // Or you can match an exact error message using a regexp like below
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
 });
