6b691e762dec72e5fd005b88250a07564cd4f609
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-27 17:31:12 +0700

    update hooks

--- Odin Projects/Full Stack JavaScript/JavaScript/React/hooks/src/App.js
+++ Odin Projects/Full Stack JavaScript/JavaScript/React/hooks/src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("black");
+  const [width, setWidth] = useState(window.innerWidth);
+  /*
+  useEffect(() => {
+    // Do something
+  }, []);
+  ! =componentDidMount
+
+  useEffect(() => {
+  // Do something
+}, [color]);
+  ! =componentDidUpdate
+ 
+  useEffect(() => {
+  // Do something
+});
+  ! =componentDidMount and componentDidUpdate
+
+useEffect(() => {
+    // Do something
+
+    return () =>{console.log()}
+  }, []);
+   != componentWillUnmount then componentDidMount
+useEffect(() => {
+    // Do something
+
+    return () =>{console.log()}
+  }, [color]);
+   != return() when color is changed then componentDidUpdate
+  */
+
+  //execute everytime component render
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+    console.log("component update");
+    console.log("add event listener");
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      console.log("remove event listener");
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]); // hook is run when only color is changed
+
+  useEffect(() => {
+    console.log("component mount");
+  }, []);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log("mount");
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
+      <div>{width}</div>
     </div>
   );
-}
+};
 
 export default App;
