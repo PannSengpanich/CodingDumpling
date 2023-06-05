fc395f8dc24f56991fd2eb7e46f0341aede5510f
Pann Sengpanich <panncraftyo@gmail.com>
2023-06-05 11:46:14 +0700

    Update ep21(RegEx).py

--- Python/ep21(RegEx).py
+++ Python/ep21(RegEx).py
@@ -1,1 +1,8 @@
 import re
+
+a = 'pann sengpanich 123-- p pa'
+print(re.search('pa', a))
+print(re.findall('pa', a))
+print(re.findall('[pa]+', a))
+print(re.findall('[pa]*', a))
+print(re.findall('[pa]', a))
