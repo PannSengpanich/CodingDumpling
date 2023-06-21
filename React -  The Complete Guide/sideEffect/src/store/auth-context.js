de00c6cb4a2e689f80657a89559fe83cac1e2284
Pann Sengpanich <panncraftyo@gmail.com>
2023-06-21 16:54:10 +0700

    update

---
+++ React -  The Complete Guide/sideEffect/src/store/auth-context.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+// React.createContext(default state)
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
