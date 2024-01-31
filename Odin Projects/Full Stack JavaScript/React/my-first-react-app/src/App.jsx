247b87a08358e6ac1bb455005f61368b7435da93
Pann Sengpanich <panncraftyo@gmail.com>
2024-01-31 17:41:21 +0700

    use signals instead of usestate and useeffect in my-first-react-app

--- Odin Projects/Full Stack JavaScript/React/my-first-react-app/src/App.jsx
+++ Odin Projects/Full Stack JavaScript/React/my-first-react-app/src/App.jsx
@@ -1,12 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { signal, computed } from "@preact/signals-react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { Greeting } from "./Components/Greeting";
-
 function App() {
   const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  const counterSignal = signal(0);
+
+  setInterval(() => {
+    counterSignal.value += 1;
+    console.log("interval");
+  }, 1000);
+
+  let a, b;
+
+  useEffect(() => {
+    // This runs after every render
+    console.log("b");
+  });
+
+  useEffect(() => {
+    // This runs only on mount (when the component appears)
+  }, []);
+
+  useEffect(() => {
+    // This runs on mount *and also* if either a or b have changed since the last render
+  }, [a, b]);
+
+  // useEffect(() => {
+  //   const key = setInterval(() => {
+  //     setCounter((count) => count + 1);
+  //   }, 1000);
+  //   console.log("c");
+  //   return () => {
+  //     clearInterval(key);
+  //   };
+  // }, []);
+  console.log("a");
 
   return (
     <>
@@ -30,7 +63,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Greeting></Greeting>
+      <p>{counterSignal} seconds have passed.</p>
     </>
   );
 }
